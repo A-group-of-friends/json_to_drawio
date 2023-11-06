5315dc2a532248f16f81f0a06ae56bc1f6398dc7
AustynT <Austyntravis9@gmail.com>
2023-11-06 14:10:18 -0700

    docs

--- class_element.py
+++ class_element.py
@@ -3,6 +3,12 @@
 from drawio_element import DrawIOElement
 
 class ClassElement(DrawIOElement):
+    """
+    Represents a class element in a DrawIO diagram.
+
+    Attributes:
+    - name (str): The name of the class.
+    """
     def __init__(self, name):
         super().__init__()
         self.elem.attrib = {'style': 'shape=class;', 'value': name}
